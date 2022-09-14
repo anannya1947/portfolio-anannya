37232082790d4355effadc432ebea42d2f1ca4ba
Anannya Mahapatra <anannya1947@gmail.com>
2022-09-14 19:52:14 +0530

    home page content

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import './home.css';
+import woman_illustration from "./woman_illustration.jpg"
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <p>Welcome!!</p>
-                <p>I am a Developer.</p>
+                <div className="home-content">
+                    <div className="intro">
+                        <h1>hello!!</h1>
+                        <p>I'm Anannya, a developer in Bangalore</p>
+                        <button>Work with Me</button>
+                    </div>
+                    <img src={woman_illustration} alt="illustration"></img>
+                </div>
             </div>
         )
     }
--- src/Main.js
+++ src/Main.js
@@ -10,16 +10,14 @@ class Main extends Component {
         return (
             <HashRouter>
                 <div>
-                    <Link className="logo" to="/">Anannya Mahapatra</Link>
+                    {/* <Link className="logo" to="/">Anannya Mahapatra</Link> */}
 
-                    <nav>
-                        <ul className="header">
-                            <li><a href="#home-page">Home</a></li>
-                            <li><a href="#projects">Projects</a></li>
-                            <li><a href="#about">About</a></li>
-                            <li><a href="#contact">Contact</a></li>
-                        </ul>
-                    </nav>
+                    <ul className="header">
+                        <li><a href="#home-page">Home</a></li>
+                        <li><a href="#projects">Projects</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
                     {/* <div className="content">
                         <Routes>
                             <Route exact path="/" element={<Home />} />
